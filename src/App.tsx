f836f4c8994180858a3b671f9740eaafc73d6ca7
hookahcoder <aptyp67@gmail.com>
2023-02-15 19:22:25 +0200

    add grid and css variables

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,36 @@ interface Mode {
 
 function App() {
   const [modes, setModes] = useState<Mode[]>([]);
-  const [selectedMode, setSelectedMode] = useState<number>(-1);
+  const [selectedModeIndex, setSelectedModeIndex] = useState<number>(-1);
   const [squares, setSquares] = useState<JSX.Element[]>([]);
   const [hoveredSquares, setHoveredSquares] = useState<number[]>([]);
+  const [gridColumns, setGridColumns] = useState<number>(5);
 
   useEffect(() => {
     fetchModes();
   }, []);
 
-  function fetchModes() {
-    fetch("https://demo7919674.mockable.io")
-      .then(response => response.json())
-      .then(setModes)
-      .catch(console.error);
+  async function fetchModes() {
+    try {
+      const response = await fetch("https://demo7919674.mockable.io");
+      const modes = await response.json();
+      setModes(modes);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   function handleModeSelection(event: React.ChangeEvent<HTMLSelectElement>) {
-    setSelectedMode(parseInt(event.target.value));
+    setSelectedModeIndex(Number(event.target.value));
   }
 
   function handleStartClick() {
-    if (selectedMode !== -1) {
-      const fieldSize = modes[selectedMode].field;
-      const newSquares = Array(fieldSize * fieldSize).fill(null)
+    if (selectedModeIndex !== -1) {
+      const fieldSize = modes[selectedModeIndex].field;
+      const newSquares = Array(fieldSize * fieldSize).fill(null);
       setSquares(newSquares);
+      setGridColumns(fieldSize);
+      setHoveredSquares([]);
     }
   }
 
@@ -43,27 +49,30 @@ function App() {
     }
   }
 
+  const selectedMode = modes[selectedModeIndex];
+
   return (
     <div>
       <h1>StarNavi: Test task</h1>
       <div>
-        <select value={selectedMode} onChange={handleModeSelection}>
+        <select value={selectedModeIndex} onChange={handleModeSelection}>
           <option value={-1}>Select a mode</option>
           {modes.map((mode, index) => (
             <option key={index} value={index}>Mode {index + 1} ({mode.field}x{mode.field})</option>
           ))}
         </select>
-        <button onClick={handleStartClick}>Start</button>
+        <button onClick={handleStartClick} disabled={selectedModeIndex === -1}>Start</button>
       </div>
       {squares.length > 0 && (
-        <div className="square-field">
+        <div className="square-field" style={{
+          gridTemplateColumns: `repeat(var(--cols, ${gridColumns}), min-content)`,
+        }}>
           {squares.map((_, index) => (
             <div
               key={index}
               className={hoveredSquares.includes(index) ? "square blue" : "square"}
               onMouseEnter={() => handleSquareHover(index)}
-            >
-            </div>
+            />
           ))}
         </div>
       )}
